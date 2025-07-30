ed878a4cb5ea6db2be3736730b9e1e8a23b2e57d
Yolke <jeremygago@hotmail.fr>
2025-07-30 19:57:18 +0200

    algo 30/07/2025

---
+++ C/Math/duplicate.py
@@ -0,0 +1,39 @@
+def duplicate(list1):
+    dict = {}
+    for num in list1:
+        if num in dict:
+            return False
+        else:
+            dict[num] = 1
+    return True
+
+print(duplicate([1, 2, 3]))     # ✅ True (pas de doublons)
+print(duplicate([1, 2, 2, 3]))  # ❌ False (2 est en double)
+
+def intersect(l1, l2):
+    return list(set(l1) & set(l2))  # intersection entre deux ensembles
+
+
+def intersect(l1, l2):
+    l1.sort()
+    l2.sort()
+    res = []
+    sizel1 = len(l1)
+    sizel2 = len(l2)
+    i = 0
+    j = 0
+    while(i < sizel1 and j < sizel2):
+        if l1[i] == l2[j]:
+            if not res or res[-1] != l1[i]:
+                res.append(l1[i])
+            i += 1
+            j += 1
+        elif l1[i] > l2[j]:
+            j += 1
+        elif l1[i] < l2[j]:
+            i += 1
+    return res
+            
+
+print(intersect([1, 2, 2, 3], [2, 2, 3]))  # [2, 3]
+print(intersect([4, 5, 6], [1, 2]))        # []
